556c8496bf829dd976da12bf41adc0bebe3eccba
Jvbass <juan.vpinoc@gmail.com>
2024-04-09 12:05:09 -0400

    Agrega ejercicio sobre bloques, alcance o scope en java

--- Ejercicios_Java/EjerciciosModulo1y2/src/main/java/cl/jpino/clase19_bloques_scope/BloqueScope.java
+++ Ejercicios_Java/EjerciciosModulo1y2/src/main/java/cl/jpino/clase19_bloques_scope/BloqueScope.java
@@ -5,15 +5,13 @@ public class BloqueScope {
         // Declaración de variables
         int x = 10;
         int y = 20;
-
         // Bloque de código
         {
             int z = 30;
             System.out.println("Dentro del bloque: x = " + x + ", y = " + y + ", z = " + z);
         }
-
         // Error: z no está definida fuera del bloque
         // System.out.println("Fuera del bloque: x = " + x + ", y = " + y + ", z = " + z);
-
     }
+
 }
